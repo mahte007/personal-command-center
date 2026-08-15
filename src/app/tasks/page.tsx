ccb2e4ef7628ae0c82a1ce3ed66ae57cf396834f
Mate Horvath <mahte007@gmail.com>
2026-08-15 14:48:12 +0200

    feat: integrate tasks page

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,13 +1,17 @@
 import { PageContainer } from "@/components/layout/page-container";
 import { PageHeader } from "@/components/layout/page-header";
+import { TasksView } from "@/features/tasks/components/tasks-view";
 
 export default function TasksPage() {
   return (
     <PageContainer>
-      <PageHeader
-        title="Tasks"
-        description="Manage everything you need to get done."
-      />
+      <div className="flex flex-col gap-8">
+        <PageHeader
+          title="Tasks"
+          description="Manage everything you need to get done."
+        />
+        <TasksView />
+      </div>
     </PageContainer>
   );
 }
